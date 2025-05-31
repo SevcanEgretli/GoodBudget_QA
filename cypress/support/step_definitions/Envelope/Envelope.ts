5fb2981a53635424611ad79d9b7a3ad61faa2905
SevcanEgretli <sevcan.egretli@adjust.com>
2025-05-31 11:37:00 +0300

    envelope update

--- cypress/support/step_definitions/Envelope/Envelope.ts
+++ cypress/support/step_definitions/Envelope/Envelope.ts
@@ -2,6 +2,7 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { EnvelopePage } from "../pageObjects/EnvelopePage";
 
 const envelopePage = new EnvelopePage();
+let uniqueEnvelopeName: string;
 
 Given("I am on the Envelopes section", () => {
   cy.visit("/home");
@@ -13,8 +14,11 @@ When('I navigate to the envelope edit page', () => {
 });
 
 When("I add a new envelope named {string} with amount {int}", (name: string, amount: number) => {
+  const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+  uniqueEnvelopeName = `${name}-${timestamp}`;
+
   envelopePage.addEnvelopeButton().click();
-  envelopePage.envelopeNameInput().type(name);
+  envelopePage.envelopeNameInput().type(uniqueEnvelopeName);
   envelopePage.envelopeAmountInput().type(amount.toString());
 });
 
@@ -29,20 +33,27 @@ Then("I should see a confirmation modal and close it", () => {
 });
 
 Then("the envelope named {string} should appear in the list", (envelopeName: string) => {
-  envelopePage.wrapper().should('contain.text', envelopeName);
+  envelopePage.wrapper().should('contain.text', uniqueEnvelopeName);
 });
 
-Then("I edit the name {string} and the amount {int}", (name: string, amount: number) => {
-  cy.get('ul#envelopes-reg > li#3').within(() => {
-    cy.get('.row-name input')
-      .clear()
-      .type(name);
+Then("I edit the name {string} and the amount {int}", (newName: string, newAmount: number) => {
+  const timestamp = new Date().toISOString().replace(/[:.]/g, "-");
+  const newUniqueName = `${newName}-${timestamp}`;
 
-    cy.get('.row-amount input')
-      .clear()
-      .type(amount.toString());
+  cy.get("ul#envelopes-reg > li").each(($el) => {
+    cy.wrap($el).within(() => {
+      cy.get(".row-name input").invoke("val").then((val) => {
+        if (val === uniqueEnvelopeName) {
+          cy.get(".row-name input").clear().type(newUniqueName);
+          cy.get(".row-amount input").clear().type(newAmount.toString());
+          cy.wrap($el).find(".row-amount input").blur();
+        }
+      });
+    });
   });
+
   envelopePage.saveChangesButton().click();
+  uniqueEnvelopeName = newUniqueName;
 });
 
 Then("I delete the envelope with id {string}", (envelopeId: string) => {
